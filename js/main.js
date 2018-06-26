df1557386516ecaefe3eb23f4d4483d230a6c230
Sobik Szymon <sobikszymon@imac-sobik.home>
2018-06-26 20:42:01 +0200

    Przewijanie do podstron + cień dla top bara

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
-    
     navBar();
-
+    navigation();
+    topShadow();
 });
+
+$(window).scroll(function() {
+    topShadow();
+})
 
 function navBar() {
     $('body').on('click', '.menuBtn', function() {
@@ -12,4 +16,27 @@ function navBar() {
     $('body').on('click', '.menuBtnHide', function() {
         $('.navBox').addClass('hide');
     });
+}
+
+function navigation() {
+    $('body').on('click', '.navigation a', function(e) {
+        var section = $(this).attr("href");
+        $('html, body').animate({
+            scrollTop: $(section).offset().top
+        });
+        $('.navBox').addClass('hide');
+        e.preventDefault();
+        return false;
+    });
+}
+
+function topShadow() { 
+    var scroll = $(window).scrollTop(),
+        pageHeight = $(window).outerHeight();
+
+    if (scroll <= pageHeight) {
+        $('.topBar').removeClass('shadow');
+    } else {
+        $('.topBar').addClass('shadow');
+    }
 }
